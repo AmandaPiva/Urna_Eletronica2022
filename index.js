78ed8a2cee3bc93cfdfd66c34a31068284c92857
Amanda Piva <78767589+AmandaPiva@users.noreply.github.com>
2022-09-06 23:07:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -215,40 +215,232 @@ var nul = 0;
     //função botão voto em branco
     function branco(){
         white++;
-        alert("vc votou em branco");
+        alert("Você votou em branco");
     }
 
     //função botão voto em branco
     function Nulo(){
         nul++;
-        alert("vc votou nulo");
+        alert("Você votou nulo");
     }
+
+    function whitenull(){
+        aviso.innerHTML = `Houve ${white} votos em Branco e <br> ${nul} votos nulo nesta eleição.`
+       }
 
    function eleito(){
     if(bolsonaro > lula && bolsonaro > marina && lula > marina && marina > ciro){
-             aviso.innerHTML = "O vencedor é o Bolsonaro"
+             aviso.innerHTML = `O vencedor é o Bolsonaro <br> Lula em segundo <br> Marina em terceiro <br> Ciro Gomes em ultimo`
+             var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/bolsonaro-campos-do-jordao.jpg";
+
+             candidato.appendChild(img);
         }
+        if(bolsonaro > lula && bolsonaro > ciro && lula > ciro && ciro > marina){
+            aviso.innerHTML = `O vencedor é o Bolsonaro <br> Lula em segundo <br> Ciro Gomes em terceiro <br> Marina em ultimo`
+
+            var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/bolsonaro-campos-do-jordao.jpg";
+
+             candidato.appendChild(img);
+
+           
+       }
         else if(bolsonaro > marina && bolsonaro > lula && marina > lula && lula > ciro){
-            aviso.innerHTML = "O vencedor é o Bolsonaro"
+            aviso.innerHTML = `O vencedor é o Bolsonaro <br> Marina em segundo <br> Lula em terceiro <br> Ciro Gomes em ultimo`
+            
+            var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/bolsonaro-campos-do-jordao.jpg";
+
+             candidato.appendChild(img);
         }
         else if(bolsonaro > ciro && bolsonaro > lula && ciro > lula && lula > marina){
-            aviso.innerHTML = "O vencedor é o Bolsonaro"
+            aviso.innerHTML = `O vencedor é o Bolsonaro <br> Ciro em segundo <br> Lula em terceiro <br> Marina em ultimo`
+
+            var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/bolsonaro-campos-do-jordao.jpg";
+
+             candidato.appendChild(img);
+
         }
         else if(bolsonaro > ciro && bolsonaro > lula && ciro > lula && marina > lula){
-            aviso.innerHTML = "O vencedor é o Bolsonaro"
+            aviso.innerHTML = `O vencedor é o Bolsonaro <br> Ciro Gomes em segundo <br> Marina em terceiro <br> Lula em ultimo`
+            
+            var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/bolsonaro-campos-do-jordao.jpg";
+
+             candidato.appendChild(img);
         }
-        //parou aqui
         else if(lula > bolsonaro && lula > marina && bolsonaro > marina && marina > ciro){
-            aviso.innerHTML = "O vencedor é o Lula"
+            aviso.innerHTML = `O vencedor é o Lula <br> Bolsonaro em segundo <br> Marina em terceiro <br> Ciro Gomes em ultimo`
+
+            var candidato = document.querySelector("#images");
+
+             var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/lula.jpg";
+
+             candidato.appendChild(img);
+
         }
-        else if(lula > bolsonaro && lula > marina && marina > bolsonaro && marina > ciro){
-            aviso.innerHTML = "O vencedor é o Lula"
+        else if(lula > ciro && lula > bolsonaro && ciro > bolsonaro && bolsonaro > marina){
+            aviso.innerHTML = `O vencedor é o Lula <br> Ciro Gomes em segundo <br> Bolsonaro em terceiro <br> Marina em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/lula.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(lula > marina && lula > bolsonaro && marina > bolsonaro && bolsonaro > ciro){
+            aviso.innerHTML = `O vencedor é o Lula <br> Marina em segundo <br> Bolsonaro em terceiro <br> Ciro Gomes em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/lula.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(lula > marina && lula > bolsonaro && marina > bolsonaro && ciro > bolsonaro){
+            aviso.innerHTML = `O vencedor é o Lula <br> Marina em segundo <br> Ciro Gomes em terceiro <br> Bolsonaro em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/lula.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(lula > ciro && lula > marina && ciro > marina && marina > bolsonaro){
+            aviso.innerHTML = `O vencedor é o lula <br> Ciro em segundo <br> Marina em terceiro <br> Bolsonaro em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/lula.jpg";
+
+             candidato.appendChild(img);
         }
         else if(marina > ciro && marina > bolsonaro && ciro > bolsonaro && bolsonaro > lula){
-            aviso.innerHTML = "A vencedora é a Marina Silva"
+            aviso.innerHTML =  `O vencedor é a Marina <br> Ciro Gomes em segundo <br> Bolsonaro em terceiro <br> Lula em ultimo`
+
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/marina.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(marina > bolsonaro && marina > ciro && bolsonaro > ciro && ciro > lula){
+            aviso.innerHTML =  `O vencedor é a Marina <br> Bolsonaro em segundo <br> Ciro Gomes em terceiro <br> Lula em ultimo`
+
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/marina.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(marina > bolsonaro && marina > lula && bolsonaro > lula && lula > ciro){
+            aviso.innerHTML =  `O vencedor é a Marina <br> Bolsonaro em segundo <br> Lula em terceiro <br> Ciro Gomes em ultimo`
+
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/marina.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(marina > lula && marina > ciro && lula > ciro && ciro > bolsonaro){
+            aviso.innerHTML =  `O vencedor é a Marina <br> Lula em segundo <br> Ciro Gomes em terceiro <br> Bolsonaro em ultimo`
+
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/marina.jpg";
+
+             candidato.appendChild(img);
+        }
+        else if(marina > ciro && marina > lula && ciro > lula && lula > bolsonaro){
+            aviso.innerHTML =  `O vencedor é a Marina <br> Ciro Gomes em segundo <br> Lula em terceiro <br> Bolsonaro em ultimo`
+
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/marina.jpg";
+
+             candidato.appendChild(img);
         }
         else if(ciro > marina && ciro > lula && marina > lula && lula > bolsonaro){
-            aviso.innerHTML = "O vencedor é o Ciro Gomes"
+            aviso.innerHTML = `O vencedor é o Ciro <br> Marina em segundo <br> Lula em terceiro <br> Bolsonaro em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/ciro.png";
+
+             candidato.appendChild(img);
         }
-        
+        else if(ciro > lula && ciro > marina && lula > marina && marina > bolsonaro){
+            aviso.innerHTML = `O vencedor é o Ciro <br> Lula em segundo <br> Marina em terceiro <br> Bolsonaro em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/ciro.png";
+
+             candidato.appendChild(img);
+        }
+        else if(ciro > bolsonaro && ciro > lula && bolsonaro > lula && lula > marina){
+            aviso.innerHTML = `O vencedor é o Ciro <br> Bolsonaro em segundo <br> Lula em terceiro <br> Marina em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/ciro.png";
+
+             candidato.appendChild(img);
+        }
+        else if(ciro > bolsonaro && ciro > marina && bolsonaro > marina && marina > lula){
+            aviso.innerHTML = `O vencedor é o Ciro <br> Bolsonaro em segundo <br> Marina em terceiro <br> Lula em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/ciro.png";
+
+             candidato.appendChild(img);
+        }
+        else if(ciro > marina && ciro > bolsonaro && marina > bolsonaro && bolsonaro > lula){
+            aviso.innerHTML = `O vencedor é o Bolsonaro <br> Lula em segundo <br> Marina em terceiro <br> Ciro Gomes em ultimo`
+            
+            var img = new Image(200, 200);  //criando a imagem
+             img.src = "img/ciro.png";
+
+             candidato.appendChild(img);
+        }
+        //empates
+        else if(bolsonaro == lula && marina > ciro){
+            aviso.innerHTML = `Houve empate entre Lula e Bolsonaro <br> Haverá segundo turno! <br> Marina ficou em segundo lugar e Ciro Gomes em terceiro`
+        }
+        else if(bolsonaro == lula && ciro > marina){
+            aviso.innerHTML = `Houve empate entre Lula e Bolsonaro <br> Haverá segundo turno! <br> Ciro Gomes ficou em segundo lugar e Marina em terceiro`
+        }
+        else if(bolsonaro == ciro && lula > marina){
+            aviso.innerHTML = `Houve empate entre Ciro e Bolsonaro <br> Haverá segundo turno! <br> Lula ficou em segundo lugar e Marina em terceiro`
+        }
+        else if(bolsonaro == ciro && marina > lula){
+            aviso.innerHTML = `Houve empate entre Ciro e Bolsonaro <br> Haverá segundo turno! <br> Marina ficou em segundo lugar e Lula em terceiro`
+        }
+        else if(bolsonaro == marina && lula > ciro){
+            aviso.innerHTML = `Houve empate entre Marina e Bolsonaro <br> Haverá segundo turno! <br> Lula ficou em segundo lugar e Ciro Gomes em terceiro`
+        }
+        else if(bolsonaro == marina && ciro > lula){
+            aviso.innerHTML = `Houve empate entre Marina e Bolsonaro <br> Haverá segundo turno! <br> Lula ficou em segundo lugar e Ciro Gomes em terceiro`
+        }
+        else if(lula == marina && bolsonaro > ciro){
+            aviso.innerHTML = `Houve empate entre Marina e Lula <br> Haverá segundo turno! <br> Bolsonaro ficou em segundo lugar e Ciro Gomes em terceiro`
+        }
+        else if(lula == marina && ciro > bolsonaro){
+            aviso.innerHTML = `Houve empate entre Marina e Lula <br> Haverá segundo turno! <br> Ciro Gomes ficou em segundo lugar e Bolsonaro em terceiro`
+        }
+        else if(lula == ciro && marina > bolsonaro){
+            aviso.innerHTML = `Houve empate entre Ciro Gomes e Lula <br> Haverá segundo turno! <br> Marina ficou em segundo lugar e Bolsonaro em terceiro`
+        }
+        else if(lula == ciro && bolsonaro > marina){
+            aviso.innerHTML = `Houve empate entre Ciro Gomes e Lula <br> Haverá segundo turno! <br> Bolsonaro ficou em segundo lugar e Marina em terceiro`
+        }
+        else if(marina == ciro && bolsonaro > lula){
+            aviso.innerHTML = `Houve empate entre Ciro Gomes e Marina <br> Haverá segundo turno! <br> Bolsonaro ficou em segundo lugar e Lula em terceiro`
+        }
+        else if(marina == ciro && lula > bolsonaro){
+            aviso.innerHTML = `Houve empate entre Ciro Gomes e Marina <br> Haverá segundo turno! <br> Lula ficou em segundo lugar e Bolsonaro em terceiro`
+        }
    }
+
+   
